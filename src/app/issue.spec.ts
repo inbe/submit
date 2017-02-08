99fca6ab7ff4e3ecd6b8c495950260373e319256
Anton Andersson <anton@inventiveit.se>
2017-02-08 22:47:32 +0000

    Added test for issue object with assertion of values

--- src/app/issue.spec.ts
+++ src/app/issue.spec.ts
@@ -4,4 +4,13 @@ describe('Issue', () => {
   it('should create an instance', () => {
     expect(new Issue()).toBeTruthy();
   });
+
+  it('should accept values in the constructor', () => {
+  let issue = new Issue({
+    user: 1,
+    description: 'Need more sun in february!'
+  });
+    expect(issue.user).toEqual(1);
+    expect(todo.description).toEqual('Need more sun in february!');
+  });
 });
